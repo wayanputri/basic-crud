1d9761f52198381b08b4b4c1cc2e67935ab6a172
wayanputri <wayanputriyanti1502@gmail.com>
2023-03-18 22:14:56 +0700

    first commit

---
+++ db_school.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2023 at 08:49 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_siswa`
+--
+
+CREATE TABLE `tb_siswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nisn` int(15) NOT NULL,
+  `nama_siswa` varchar(50) NOT NULL,
+  `jenis_kelamin` varchar(15) NOT NULL,
+  `foto_siswa` varchar(50) NOT NULL,
+  `alamat` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_siswa`
+--
+
+INSERT INTO `tb_siswa` (`id_siswa`, `nisn`, `nama_siswa`, `jenis_kelamin`, `foto_siswa`, `alamat`) VALUES
+(25, 99999928, 'Citra', 'Perempuan', '99999928.jpg', 'kLIAAA      		      		      '),
+(29, 910101, 'uyu', 'Laki-laki', '9101010.png', 'io      		      '),
+(30, 9090, 'rt', 'Laki-laki', '9090.jpeg', 'io      '),
+(31, 23, 'uia', 'Perempuan', '0023.jpeg', 'opa      '),
+(32, 999, 'koko', 'Perempuan', '999.jpg', 'kok	      '),
+(33, 90131, 'iji', 'Laki-laki', '90131.jpg', 'ggg	      ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'db_school';
+	$conn=mysqli_connect($host,$user,$pass,$db);
+	if($conn){
+		//echo "Koneksi Berhasil";
+	}
+
+	mysqli_select_db($conn, $db);
+?>
